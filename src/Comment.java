815a04690bb63f8b8e931184ac2eaa9938ad1bf6
wongkaho <wongkaho@192.168.1.6>
2018-01-26 17:06:40 +0800

    add post and comment object

---
+++ src/Comment.java
@@ -0,0 +1,32 @@
+
+public class Comment {
+	private String id;
+	private String i;
+	private String title;
+	private double score;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getI() {
+		return i;
+	}
+	public void setI(String string) {
+		this.i = string;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double d) {
+		this.score = d;
+	}
+}
---
+++ src/Post.java
@@ -0,0 +1,26 @@
+
+public class Post {
+	private String id;
+	private String title;
+	private float score;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
+	
+}
